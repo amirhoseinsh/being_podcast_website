84ce61903643afdbc4be5a3580008afe9bc7d5e2
amirhoseinsh <amirhhh.shirani@yahoo.com>
2024-07-30 20:10:51 +0330

    update episode page

---
+++ app/[lang]/episodes/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+
+type params = {
+    lang: string
+}
+
+
+export default function RootLayout({
+  children, params
+}: {
+  children: React.ReactNode;
+  params: params
+}) {
+  return (
+   <section>
+        <div className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+        </div>
+      </section>
+  );
+}
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,6 +4,10 @@ import Particles from "../components/particles";
 import Image from "next/image";
 import { getDictionary } from './dictionaries'
 
+const navigation = [
+  {name: "Episodes", href: "/episodes"},
+  {name: "Document", href: "/document"}
+]
 type params = {
   lang: string
 }
@@ -13,7 +17,7 @@ export default async function Page({ params }: {params: params}) {
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      {/* <nav className="my-16 animate-fade-in">
+      <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -25,7 +29,7 @@ export default async function Page({ params }: {params: params}) {
             </Link>
           ))}
         </ul>
-      </nav> */}
+      </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
