b82020c08746aa84a1032f4580c91c9f289b36f7
amirhoseinsh <amirhhh.shirani@yahoo.com>
2024-08-22 15:29:58 +0330

    remove nav const

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
 export default function Navigation() {
-    const []
     return(
         <div></div>
     )
