44d546b973b19c908297efc2b57e34ff3e86ef18
amirhoseinsh <amirhhh.shirani@yahoo.com>
2024-08-22 15:38:37 +0330

    responsive design episodes

--- app/[lang]/episodes/page.tsx
+++ app/[lang]/episodes/page.tsx
@@ -13,28 +13,28 @@ export default async function Episodes({ params }: { params: params }) {
           <h1 className="text-white text-5xl">{dict.episode_page.main}</h1>
         </div>
           <div className="mt-12 mb-4 flex flex-col w-full items-center justify-center">  
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf3" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf3" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf4" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf4" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf5" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf5" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf6" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf6" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf7" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf7" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf8" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf8" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf9" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bf9" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
           <div className="m-4 flex flex-col w-full items-center justify-center">
-            <iframe className="w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bfa" frameBorder="0" width="100%" height="80px"></iframe>
+            <iframe className="w-full px-2 md:w-1/3" src="https://embed.acast.com/66c35ec6c657d26e5df049b9/66c35ecbc657d26e5df04bfa" frameBorder="0" width="100%" height="80px"></iframe>
           </div>
     </div>
   );
